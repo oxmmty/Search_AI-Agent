03fbb105b596840671ce485f741f37d977bf6b3d
oxmmty <oxmmty@gmail.com>
2025-09-10 07:08:36 +0900

    update db conn

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
+const mongoose = require('mongoose');
+const { DB_PORT } = require('./configs/database');
 
 const appRouter = require('./routes');
 
@@ -18,6 +20,18 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../uploads')));
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(DB_PORT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connecting to database successful');
+  })
+  .catch((err) => console.error('Could not connect to mongo DB', err));
 
 app.get('/health', (req, res) => {
   res.status(200).send('Cool!');
