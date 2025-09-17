ede841cf6327fe1f5ae4b697fa2442e13bfd166b
oxmmty <oxmmty@gmail.com>
2025-09-18 01:32:29 +0900

    remove keys

--- src/scrapping-extension/config.js
+++
@@ -1,4 +0,0 @@
-module.exports.DB_PORT = 'mongodb://127.0.0.1:27017/realestate';
-// module.exports.DB_PORT = 'mongodb+srv://admin:K5p04YXJC7lgH6Nj@estatecluster.zrhrst0.mongodb.net/realestate?retryWrites=true&w=majority&appName=estatecluster';
-module.exports.OPENAI_API_KEY = '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';
-module.exports.OPENAI_MODEL = 'gpt-4o-mini';
